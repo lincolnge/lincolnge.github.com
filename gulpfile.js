82d8576bacc9183938371d135783f8609669d1c2
lincolnge <326684793@qq.com>
2017-01-11 13:23:53 +0800

    Update: 优化图片。直接执行 gulp image 即可。

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,75 @@ var browserSync = require('browser-sync');
 var sass        = require('gulp-sass');
 var prefix      = require('gulp-autoprefixer');
 var cp          = require('child_process');
+var imagemin    = require('gulp-imagemin');
+var jpegtran    = require('imagemin-jpegtran');
+var pngquant    = require('imagemin-pngquant');
+var imageResize = require('gulp-image-resize');
 
 var messages = {
     jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
 };
+
+// gulp.task('copy', () =>
+//     gulp.src('files/images/**/*')
+//       .pipe(gulp.dest('files/images_ori'))
+// );
+
+// 任务：压缩jpg
+gulp.task('jpgmin', () =>
+    gulp.src(['files/images_ori/**/*.jpg', 'files/images_ori/**/*.jepg'])
+      .pipe(imagemin({
+          progressive: true,
+          use: [jpegtran()]
+      }))
+      .pipe(gulp.dest('files/images'))
+);
+
+// 任务：压缩png
+gulp.task('pngmin', () =>
+    gulp.src('files/images_ori/**/*.png')
+        .pipe(imagemin({
+            quality: '65-80',
+            speed: 4,
+            use:[pngquant()]
+        }))
+        .pipe(gulp.dest('files/images'))
+);
+
+gulp.task('imagemin2', ['jpgmin', 'pngmin'])
+
+/**
+ * 安装必要的依赖。
+ * brew install imagemagick
+ * brew install graphicsmagick
+ **/
+gulp.task('resize', function () {
+    return gulp.src('files/images_ori/**/*')
+        .pipe(imageResize({
+            width: 700
+        }))
+        .pipe(gulp.dest('files/images'));
+});
+
+gulp.task('imagemin', () =>
+    gulp.src('files/images_ori/**/*')
+        .pipe(imagemin({
+            progressive: true, // 无损压缩JPG图片
+        }))
+        .pipe(gulp.dest('files/images'))
+);
+
+gulp.task('image', () =>
+    gulp.src('files/images_ori/**/*')
+        .pipe(imageResize({
+            width: 700
+        }))
+        .pipe(imagemin({
+            progressive: true, // 无损压缩JPG图片
+            use:[pngquant()],
+        }))
+        .pipe(gulp.dest('files/images'))
+);
 
 /**
  * Build the Jekyll Site
