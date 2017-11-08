3eecf955e133f855bfcbd2a5d11987c80bd96e76
lincolnge <326684793@qq.com>
2017-11-09 00:24:56 +0800

    Update: Change SASS to Less.

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var imagemin    = require('gulp-imagemin');
 var jpegtran    = require('imagemin-jpegtran');
 var pngquant    = require('imagemin-pngquant');
 var imageResize = require('gulp-image-resize');
+var less        = require('gulp-less');
 
 var messages = {
     jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
@@ -92,7 +93,7 @@ gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
 /**
  * Wait for jekyll-build, then launch the Server
  */
-gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
+gulp.task('browser-sync', ['css', 'jekyll-build'], function() {
     browserSync({
         server: {
             baseDir: '_site'
@@ -104,6 +105,17 @@ gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
 /**
  * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
  */
+
+gulp.task('less', function () {
+    return gulp.src('assets/themes/twitter-lin/less/*.less')
+        .pipe(less({}))
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(gulp.dest('_site/assets/themes/twitter-lin/css'))
+        .pipe(browserSync.reload({stream:true}))
+        .pipe(gulp.dest('assets/themes/twitter-lin/css'));
+});
+
+// 不再用 sass 啦。
 gulp.task('sass', function () {
     return gulp.src('assets/themes/twitter-lin/scss/*.scss')
         .pipe(sass({
@@ -116,13 +128,18 @@ gulp.task('sass', function () {
         .pipe(gulp.dest('assets/themes/twitter-lin/css'));
 });
 
+gulp.task('css', function () {
+    // return gulp.run('sass');
+    return gulp.run('less');
+});
+
 /**
  * Watch scss files for changes & recompile
  * Watch html/md files, run jekyll & reload BrowserSync
  */
 gulp.task('watch', function () {
-    gulp.watch('assets/themes/twitter-lin/scss/*.scss', ['sass']);
-    gulp.watch(['*.md', '_layouts/*.html', '_posts/**/*', '*.html', '_config.yml', 'assets/themes/twitter-lin/js/*.js'], ['jekyll-rebuild']);
+    gulp.watch('assets/themes/twitter-lin/less/*.less', ['css']);
+    gulp.watch(['*.md', '_layouts/*.html', '_posts/**/*', '**/**.html', '_config.yml', 'assets/themes/twitter-lin/js/*.js'], ['jekyll-rebuild']);
 });
 
 /**
